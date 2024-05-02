5661eb6b33221fe8a078b0e3aacfbd8563ac1761
OmarThierno <thiernomarndiaye76@gmail.com>
2024-05-03 01:51:17 +0200

    feat: done footer section

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -14,22 +14,23 @@ export default {
       <div class="container">
         <div class="icon">
           <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-
+          <span>DIGITAL COMICS</span>
         </div>
         <div class="icon">
           <img src="../assets/img/buy-comics-merchandise.png" alt="">
-
+          <span>DC ERCHANDISE</span>
         </div>
         <div class="icon">
           <img src="../assets/img/buy-comics-subscriptions.png" alt="">
-
+          <span>SUBCRIVEPTION</span>
         </div>
         <div class="icon">
-          <img src="../assets/img/buy-comics-shop-locator.png" alt="">
-
+          <img class="position-icon" src="../assets/img/buy-comics-shop-locator.png" alt="">
+          <span>COMIC SHOP LOCATOR</span>
         </div>
         <div class="icon">
           <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+          <span>DC POWER VISA</span>
 
         </div>
       </div>
@@ -90,17 +91,49 @@ export default {
       </div>
     </section>
     <section class="sign-up-section">
-      <div class="container">ashashc</div>
+      <div class="container">
+        <div class="sign-up"><a href="">SIGN UP NOW!</a></div>
+        <div class="social-icon">
+          <span><a href="">FOLOW US</a></span>
+          <ul>
+            <li>
+              <a href="">
+                <img src="../assets/img/footer-youtube.png" alt="">
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="../assets/img/footer-youtube.png" alt="">
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="../assets/img/footer-youtube.png" alt="">
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="../assets/img/footer-pinterest.png" alt="">
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <img src="../assets/img/footer-periscope.png" alt="">
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </section>
   </footer>
 
 </template>
 
-<style lang='scss'>
+<style lang='scss' scoped>
 @use "../style/partials/mixin" as *;
 @use "../style/partials/variables" as *;
 
-footer {  
+footer {
   .icon-section {
     background-color: $primary-color;
     padding: 20px 0;
@@ -110,9 +143,25 @@ footer {
       // height: 100%;
       @include flex(row, space-around, center);
 
-      img {
-        width: 40px;
+      .icon {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        color: white;
+
+        img {
+          width: 40px;
+          &.position-icon {
+            width: 25px;
+          }
+        }
+
+        span {
+          font-size: 0.6rem;
+        }
+
       }
+
     }
   }
 
@@ -124,28 +173,29 @@ footer {
 
     .container {
       display: flex;
-  
+
       .footer-list {
-        width: 40%;
+        width: 30%;
         display: flex;
         justify-content: space-between;
-  
+
         h4 {
           font-size: 1rem;
           color: white;
         }
-    
+
         ul {
           list-style-type: none;
           color: grey;
           font-size: 0.7rem;
         }
       }
-  
+
       .logo-footer {
         text-align: center;
-        width: 60%;
+        width: 70%;
         position: relative;
+
         img {
           display: block;
           width: 300px;
@@ -155,15 +205,51 @@ footer {
         }
       }
 
-      
+
     }
-    
+
   }
+
   .sign-up-section {
+    padding: 20px 0;
+    position: relative;
     z-index: 2;
     background-color: #303030;
-    min-height: 100px;
-    text-align: center;
+    @include flex(row, center, center);
+
+    .sign-up {
+      display: block;
+      padding: 10px 20px;
+      border: 2px solid $primary-color;
+      color: white;
+    }
+
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+    .container {
+      @include flex(row, space-between, center);
+
+      .social-icon {
+        @include flex(row, center, center);
+        color: $primary-color;
+    
+        ul {
+          list-style-type: none;
+          @include flex(row, center, center);
+          gap: 15px;
+          margin-left: 15px;
+    
+          img {
+            width: 30px
+          }
+        }
+  
+      }
+    }
+
+
   }
 }
 </style>
