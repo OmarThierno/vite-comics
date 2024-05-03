a2038b41114ce17e8de975f0f0c1283bc541b27f
OmarThierno <thiernomarndiaye76@gmail.com>
2024-05-03 16:11:12 +0200

    feat: add main section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,85 @@
 <script>
+import ProductCard from './ProductCard.vue'
   export default {
+    components: {
+      ProductCard,
+    },
     data() {
       return{
-        
+        films: [
+          {
+              "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+              "price": "$19.99",
+              "series": "Action Comics",
+              "type": "comic book",
+          },
+          {
+              "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+              "price": "$3.99",
+              "series": "American Vampire 1976",
+              "type": "comic book",
+          },
+          {
+              "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+              "price": "$16.99",
+              "series": "Aquaman",
+              "type": "graphic novel",
+          },
+          {
+              "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+              "price": "$2.99",
+              "series": "Batgirl",
+              "type": "comic book",
+          },
+          {
+              "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+              "price": "$3.99",
+              "series": "Batman",
+              "type": "comic book",
+          },
+          {
+              "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+              "price": "$2.99",
+              "series": "Batman Beyond",
+              "type": "comic book",
+          },
+          {
+              "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+              "price": "$3.99",
+              "series": "Batman/Superman",
+              "type": "comic book",
+          },
+          {
+              "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+              "price": "$4.99",
+              "series": "Batman/Superman Annual",
+              "type": "comic book",
+          },
+          {
+              "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+              "price": "$5.99",
+              "series": "Batman: The Joker War Zone",
+              "type": "comic book",
+          },
+          {
+              "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+              "price": "$6.99",
+              "series": "Batman: Three Jokers",
+              "type": "comic book",
+          },
+          {
+              "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+              "price": "$4.99",
+              "series": "Batman: White Knight Presents: Harley Quinn",
+              "type": "comic book",
+          },
+          {
+              "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+              "price": "$16.99",
+              "series": "Catwoman",
+              "type": "graphic novel",
+          },
+        ]
       }
     }
   }
@@ -10,8 +87,17 @@
 
 <template>
   <main>
+    <div class="hero"></div>
+
     <div class="container">
-      <div> - - > Content goes here <--</div>
+      <div class="row">
+        <div v-for="curFilm in films" class="col">
+          <ProductCard :cardImage="curFilm.thumb" :cardTitle="curFilm.series"/>
+        </div>
+      </div>
+      <div class="btn-load-mode">
+        <a href="">Load mode</a>
+      </div>
     </div>
   </main>
   
@@ -23,14 +109,43 @@
 
   main {
     background-color: black;
-    // debug
-    @include flex(row, center, center);
-    min-height: 200px;
+
+    .hero {
+      min-height: 20vh;
+      background-image: url('../assets/img/jumbotron.jpg');
+      background-size: cover;
+    }
 
     .container {
+      padding: 30px 0;
 
-      div {
-        color: white;
+      .row {
+        @include flex(row, space-between, stretch);
+        flex-wrap: wrap;
+        gap: 10px;
+        margin-bottom: 20px;
+
+        .col {
+          width: calc(100% / 6 - 50px / 6);
+        }
+
+      }
+      .btn-load-mode {
+        @include flex(row, center, center);
+
+        a {
+          color: white;
+          display: inline-block;
+          text-decoration: none;
+          background-color: $primary-color;
+          padding: 10px 40px;
+          transition: background-color 0.15s, color 0.15s;
+
+          &:hover {
+            background-color: white;
+            color: $primary-color;
+          }
+        }
       }
     }
   }
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    props: {
+        cardImage: String,
+        cardTitle: String,
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <img :src="cardImage" :alt="cardTitle">
+        <h4>{{ cardTitle }}</h4>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .card {
+        img {
+            max-width: 100%;
+            aspect-ratio: 1;
+            object-fit: cover;
+            object-position: left top;
+        }
+
+        h4 {
+            font-size: 0.7rem;
+            color: rgb(170, 170, 170);
+        }
+    }
+</style>
